2ccc69f20843e3506dc55069b839ecd16fcc31d8
Benjith Kizhisseri <mpbenjith@gmail.com>
2018-05-19 20:04:06 +0530

    CI complete updatrion 3.1.8
    CI all files and HMVC updated into latest version and HMVC works fine

--- application/modules/Dashboard/controllers/Dashboard.php
+++ application/modules/Dashboard/controllers/Dashboard.php
@@ -3,9 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends MX_Controller {
 
-	
+	function __construct() {
+        parent::__construct();
+        $this->load->database();
+        $this->load->library('session');
+    }
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//redirect to dashboard 
+		$this->dashboard(); 
+	}
+	public function dashboard(){
+		if($this->session->userdata('superuser')){
+			echo "superuser found";
+		}
+		else {
+			echo "no admin found";
+		}
 	}
 }
