c34ad30e03a22d6197b9bfedbb87644917da8ef2
Mubeen Ali <cmubeenali@gmail.com>
2018-05-23 15:52:20 +0530

    login model altered
    sessions

--- application/modules/Dashboard/models/Mdl_login.php
+++ application/modules/Dashboard/models/Mdl_login.php
@@ -16,19 +16,17 @@ class Mdl_login extends CI_Model {
  	$username = $this->input->post('UserName');
  	$password = $this->input->post("PassWord");
 	
-	$this->db->where('Name',$username);
-	$this->db->where('Password',$password);
-	$this->db->where('Status',1);
-	$qry_getuserdata = $this->db->get('tbl_users');
+	$this->db->where('userName',$username);
+	$this->db->where('password',$password);	
+	$qry_getuserdata = $this->db->get('user_tbl');
 	
-
-	if($qry_getuserdata->num_rows() == 1){
+	if($qry_getuserdata->num_rows() > 0){
 	$result=  $qry_getuserdata->result();
-	$userid=  $result[0]->Id;
-	$usertype=$result[0]->Usertype; 
-		$this->session->set_userdata('user',$username);
-		$this->session->set_userdata('userid',$userid);
-		$this->session->set_userdata('usertype',$usertype);
+	$userid=  $result[0]->userId;
+	$usertype=$result[0]->userType; 
+		$this->session->set_userdata('userName',$username);
+		$this->session->set_userdata('userId',$userid);
+		$this->session->set_userdata('userType',$usertype);
 		return true;
 	}
 	else {
