333a52190fb779061251d68ee3bda08f69bddb5e
Mubeen Ali <cmubeenali@gmail.com>
2018-05-28 21:29:14 +0530

    final change 2

--- application/modules/General/views/users.php
+++ application/modules/General/views/users.php
@@ -144,178 +144,186 @@
 <script src="<?php echo asset_url(); ?>/vendors/bower_components/jszip/dist/jszip.min.js"></script>
 <script src="<?php echo asset_url(); ?>/vendors/bower_components/datatables.net-buttons/js/buttons.html5.min.js"></script>
 
-<<<<<<< HEAD
-<script>    
-=======
 <script>
-    $( document ).ready(function(){
-    $('.dataTables_buttons ').remove();
-    $('#data-table_length select option').css('background-color','#020203');
-});
->>>>>>> 5c74da9e786f6c7cbf00bee6c167674ddbc48ed7
-	function clear(){
-		$('#username').val('');
-		$('#email').val('');
-		$('#userType option:selected').val('User');
-		$('#password').val('');
-		$('#username').focus();
-	}
-    function switchTabNew(){
-        $('#navList').removeClass('active');
-        $('#navList').attr('aria-expanded','false')
-        $('#navNew').addClass('nav-link active');
-        $('#navNew').attr('aria-expanded','true')  
-        $('#list').attr('aria-expanded','false')                    
-        $('#addnew').attr('aria-expanded','true')
-        $('#list').removeClass('active show');
-        $('#addnew').addClass('active show');
-    }
-	function notify(from, align, icon, type, animIn, animOut, msg){
-                $.notify({
-                    icon: icon,
-                    title: 'Warning : ',
-                    message: msg,
-                    url: ''
-                },{
-                    element: 'body',
-                    type: type,
-                    allow_dismiss: true,
-                    placement: {
-                        from: from,
-                        align: align
-                    },
-                    offset: {
-                        x: 20,
-                        y: 20
-                    },
-                    spacing: 10,
-                    z_index: 1031,
-                    delay: 2500,
-                    timer: 1000,
-                    url_target: '_blank',
-                    mouse_over: false,
-                    animate: {
-                        enter: animIn,
-                        exit: animOut
-                    },
-                    template:   '<div data-notify="container" class="alert alert-dismissible alert-{0} alert--notify" role="alert" style="background-color:#ccbd0e;">' +
-                                    '<span data-notify="icon"></span> ' +
-                                    '<span data-notify="title" style="color:#000;">{1}</span> ' +
-                                    '<span data-notify="message" style="color:#000;">{2}</span>' +
-                                    '<div class="progress" data-notify="progressbar">' +
-                                        '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
-                                    '</div>' +
-                                    '<a href="{3}" target="{4}" data-notify="url"></a>' +
-                                    '<button type="button" aria-hidden="true" data-notify="dismiss" class="close"><span>×</span></button>' +
-                                '</div>'
-                });
-            }
+    $(document).ready(function () {
+    	$('.dataTables_buttons ').remove();
+    	$('#data-table_length select option').css('background-color', '#020203');
+    });
 
-	$('#btnSave').click(function(){
-		if($('#username').val()==""){
-			var animIn,animOut;
-			notify("top", "right", "fa fa-comments", "inverse", animIn, animOut,'Username is required!');
-			$('#username').focus();
-		}
-		else if($('#password').val()==""){
-			var animIn,animOut;
-			notify("top", "right", "fa fa-comments", "inverse", animIn, animOut,'Password is required!');
-			$('#password').focus();
-		}
-		else{
-            var userId=0
-			if($('#hidUserId').val()=="")
-                userId=<?php echo $userInfo['userId'] ?>;
-            else
-                userId=$('#hidUserId').val();                
-			var msg="";
-			if(userId==0)
-				msg="Saved successfully";
-			else if(userId>0)
-				msg="Updated successfully";
-			$.ajax({
-				url:'<?php echo site_url('General/Users')?>',
-				datatype:'json',
-				data:{userId: userId,
-					username: $('#username').val(),
-					email: $('#email').val(),
-					userType: $('#userType option:selected').val(),
-					password: $('#password').val()},
-				method: 'post',
-				success: function(resp){
-					swal({
-						title: 'Users',
-						text: msg,
-						type: 'success',
-						buttonsStyling: false,
-                        showConfirmButton: false,
-						confirmButtonClass: 'btn btn-sm btn-light',
-						background: 'rgba(0, 0, 0, 0.96)'
-					})					
-					setTimeout(function(){
-						location.reload();
-					}, 500);					
-				}
-			});
-		}
-	});
+    function clear() {
+    	$('#username').val('');
+    	$('#email').val('');
+    	$('#userType option:selected').val('User');
+    	$('#password').val('');
+    	$('#username').focus();
+    }
 
-    function editUser(userId){
-        if(userId>0){            
-            $.ajax({
-                url:'<?php echo site_url('General/userViewById') ?>',
-                datatype:'json',
-                data:{userId:userId},
-                method:'post',
-                success: function(resp){           
-                    switchTabNew();                             
-                    var user=$.parseJSON(resp);
-                    $('#username').val(user['userName']);
-                    $('#email').val(user['email']);
-                    $('#userType option:selected').val(user['userType']);
-                    $('#password').val(user['password']);
-                    $('#btnSave').html('Update');
-                    $('#hidUserId').val(user['userId']);
-                }
-            });
-        }
+    function switchTabNew() {
+    	$('#navList').removeClass('active');
+    	$('#navList').attr('aria-expanded', 'false')
+    	$('#navNew').addClass('nav-link active');
+    	$('#navNew').attr('aria-expanded', 'true')
+    	$('#list').attr('aria-expanded', 'false')
+    	$('#addnew').attr('aria-expanded', 'true')
+    	$('#list').removeClass('active show');
+    	$('#addnew').addClass('active show');
     }
-    function deleteUser(userId){
-        if(userId>0){            
-            swal({
-                title: 'Are you sure?',
-                text: 'This user will be deleted!',
-                type: 'warning',
-                showCancelButton: true,
-                buttonsStyling: false,
-                confirmButtonClass: 'btn btn-danger',
-                confirmButtonText: 'Yes, delete it!',
-                cancelButtonClass: 'btn btn-light',
-                background: 'rgba(0, 0, 0, 0.96)'
-                }).then(function(){
-                    $.ajax({
-                        url:'<?php echo site_url('General/userDeleteById') ?>',
-                        datatype:'json',
-                        data:{userId:userId},
-                        method:'post',
-                        success: function(resp){           
-                            swal({
-                                    title: 'Users',
-                                    text: resp,
-                                    type: 'success',
-                                    buttonsStyling: false,
-                                    showConfirmButton: false,
-                                    confirmButtonClass: 'btn btn-sm btn-light',
-                                    background: 'rgba(0, 0, 0, 0.96)'
-                                })					
-                                setTimeout(function(){
-                                    location.reload();
-                                }, 500);	   
-                        }
-                    });
-                });       
-        }
+
+    function notify(from, align, icon, type, animIn, animOut, msg) {
+    	$.notify({
+    		icon: icon,
+    		title: 'Warning : ',
+    		message: msg,
+    		url: ''
+    	}, {
+    		element: 'body',
+    		type: type,
+    		allow_dismiss: true,
+    		placement: {
+    			from: from,
+    			align: align
+    		},
+    		offset: {
+    			x: 20,
+    			y: 20
+    		},
+    		spacing: 10,
+    		z_index: 1031,
+    		delay: 2500,
+    		timer: 1000,
+    		url_target: '_blank',
+    		mouse_over: false,
+    		animate: {
+    			enter: animIn,
+    			exit: animOut
+    		},
+    		template: '<div data-notify="container" class="alert alert-dismissible alert-{0} alert--notify" role="alert" style="background-color:#ccbd0e;">' +
+    			'<span data-notify="icon"></span> ' +
+    			'<span data-notify="title" style="color:#000;">{1}</span> ' +
+    			'<span data-notify="message" style="color:#000;">{2}</span>' +
+    			'<div class="progress" data-notify="progressbar">' +
+    			'<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+    			'</div>' +
+    			'<a href="{3}" target="{4}" data-notify="url"></a>' +
+    			'<button type="button" aria-hidden="true" data-notify="dismiss" class="close"><span>×</span></button>' +
+    			'</div>'
+    	});
     }
+
+    $('#btnSave').click(function () {
+    	if ($('#username').val() == "") {
+    		var animIn, animOut;
+    		notify("top", "right", "fa fa-comments", "inverse", animIn, animOut, 'Username is required!');
+    		$('#username').focus();
+    	} else if ($('#password').val() == "") {
+    		var animIn, animOut;
+    		notify("top", "right", "fa fa-comments", "inverse", animIn, animOut, 'Password is required!');
+    		$('#password').focus();
+    	} else {
+    		var userId = 0
+    		if ($('#hidUserId').val() == "")
+    			userId = <?php echo $userInfo['userId'] ?>;
+    		else
+    			userId = $('#hidUserId').val();
+    		var msg = "";
+    		if (userId == 0)
+    			msg = "Saved successfully";
+    		else if (userId > 0)
+    			msg = "Updated successfully";
+    		$.ajax({
+    			url: '<?php echo site_url('
+    			General / Users ')?>',
+    			datatype: 'json',
+    			data: {
+    				userId: userId,
+    				username: $('#username').val(),
+    				email: $('#email').val(),
+    				userType: $('#userType option:selected').val(),
+    				password: $('#password').val()
+    			},
+    			method: 'post',
+    			success: function (resp) {
+    				swal({
+    					title: 'Users',
+    					text: msg,
+    					type: 'success',
+    					buttonsStyling: false,
+    					showConfirmButton: false,
+    					confirmButtonClass: 'btn btn-sm btn-light',
+    					background: 'rgba(0, 0, 0, 0.96)'
+    				})
+    				setTimeout(function () {
+    					location.reload();
+    				}, 500);
+    			}
+    		});
+    	}
+    });
+
+    function editUser(userId) {
+    	if (userId > 0) {
+    		$.ajax({
+    			url: '<?php echo site_url('
+    			General / userViewById ') ?>',
+    			datatype: 'json',
+    			data: {
+    				userId: userId
+    			},
+    			method: 'post',
+    			success: function (resp) {
+    				switchTabNew();
+    				var user = $.parseJSON(resp);
+    				$('#username').val(user['userName']);
+    				$('#email').val(user['email']);
+    				$('#userType option:selected').val(user['userType']);
+    				$('#password').val(user['password']);
+    				$('#btnSave').html('Update');
+    				$('#hidUserId').val(user['userId']);
+    			}
+    		});
+    	}
+    }
+
+    function deleteUser(userId) {
+    	if (userId > 0) {
+    		swal({
+    			title: 'Are you sure?',
+    			text: 'This user will be deleted!',
+    			type: 'warning',
+    			showCancelButton: true,
+    			buttonsStyling: false,
+    			confirmButtonClass: 'btn btn-danger',
+    			confirmButtonText: 'Yes, delete it!',
+    			cancelButtonClass: 'btn btn-light',
+    			background: 'rgba(0, 0, 0, 0.96)'
+    		}).then(function () {
+    			$.ajax({
+    				url: '<?php echo site_url('
+    				General / userDeleteById ') ?>',
+    				datatype: 'json',
+    				data: {
+    					userId: userId
+    				},
+    				method: 'post',
+    				success: function (resp) {
+    					swal({
+    						title: 'Users',
+    						text: resp,
+    						type: 'success',
+    						buttonsStyling: false,
+    						showConfirmButton: false,
+    						confirmButtonClass: 'btn btn-sm btn-light',
+    						background: 'rgba(0, 0, 0, 0.96)'
+    					})
+    					setTimeout(function () {
+    						location.reload();
+    					}, 500);
+    				}
+    			});
+    		});
+    	}
+    }
+
 
 </script>
 
