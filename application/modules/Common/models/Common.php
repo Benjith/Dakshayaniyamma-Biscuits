a94e07b29a51ab0d091bd7dd8d54d5892aa46067
Mubeen Ali <cmubeenali@gmail.com>
2018-05-30 13:33:49 +0530

    Created new module named common for executing common queries

--- application/modules/Common/models/Common.php
+++ application/modules/Common/models/Common.php
@@ -1,6 +1,10 @@
 <?php
 
-class Mdl_UnitConversion extends CI_Model
+class Mdl_Common extends CI_Model
 {
-    
+    function BankAndCashSelectFill(){
+        $query= $this->db->query('select t1.ledgerId,t1.ledgerName from ledger_tbl t1 
+        where t1.accountId in(select accountId from (select accountId, 1 as hierarchyLevel from account_tbl where accountId in(44,48))as t2)');
+        return $query->result_array();
+    }
 }
